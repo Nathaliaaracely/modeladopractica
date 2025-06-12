dc78fa1aa66ea438369ebe76a032235c93a8599e
Nathalia Aracely <nathaliaaracely.17@gmail.com>
2025-06-12 17:10:24 -0500

    Actualizar puerto de ejecución de la aplicación a 3000

--- playground.py
+++ playground.py
@@ -40,4 +40,4 @@ app = Playground(agents=[web_agent, finance_agent]).get_app()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7777)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
