bf45069964caa5bccc483a4ceda78150cba63af8
Nathalia Aracely <nathaliaaracely.17@gmail.com>
2025-06-12 17:08:17 -0500

    Actualizar puerto de ejecución de la aplicación a 7777

--- playground.py
+++ playground.py
@@ -40,4 +40,4 @@ app = Playground(agents=[web_agent, finance_agent]).get_app()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("playground:app", host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=7777)
